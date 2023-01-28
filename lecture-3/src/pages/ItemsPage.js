afe3b115eea640aa499aef91309375d68b848bdb
MJ <80272444+minai621@users.noreply.github.com>
2023-01-28 06:45:46 +0000

    lecture-3

---
+++ lecture-3/src/pages/ItemsPage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import BannerImage from '../components/BannerImage';
+import ItemGrid from '../components/ItemGrid';
+import main_items from '../assets/main-items.jpg';
+
+function ItemsPage(props) {
+  const [items, setItems] = useState([]);
+
+  const getItems = () => {
+    axios
+      .get(
+        'https://minai621-special-invention-x44p94wpxj92pxpq-3001.preview.app.github.dev/items'
+      )
+      .then((success) => {
+        console.log(success.data);
+        setItems(success.data);
+      });
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  return (
+    <div className='ItemsPage -mt-16'>
+      <BannerImage text={'Items'} image={main_items} />
+      <div className='container mx-auto py-24'>
+        <ItemGrid data={items} />
+      </div>
+    </div>
+  );
+}
+
+export default ItemsPage;
