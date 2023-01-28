a743d4fddde30c61654852cc2fdaa33df016b66a
MJ <80272444+minai621@users.noreply.github.com>
2023-01-28 14:37:10 +0000

    code optimize

--- lecture-4/src/components/PhotoItem.js
+++ lecture-4/src/components/PhotoItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import { showModal } from '../redux/imageModal';
+import { setBgColor, showModal } from '../redux/imageModal';
 import LazyLoad from 'react-lazyload';
+import { getAverageColorOfImage } from '../utils/getAverageColorOfImage';
 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
 
-  const openModal = () => {
+  const openModal = e => {
     dispatch(showModal({ src: urls.full, alt }));
+    const averageColor = getAverageColorOfImage(e.target);
+    dispatch(setBgColor(averageColor));
   };
 
   return (
