68e75eef6c64754727a5ff04ace8506ba1e8534b
MJ <80272444+minai621@users.noreply.github.com>
2023-01-28 13:39:23 +0000

    loading FOIT Font by fontfaceobserver

--- lecture-3/src/components/BannerVideo.js
+++ lecture-3/src/components/BannerVideo.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import video_webm from '../assets/_banner-video.webm';
+import video from '../assets/_banner-video.mp4';
+import FontFaceObserver from 'fontfaceobserver';
 
 function BannerVideo() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  const font = new FontFaceObserver('BMYEONSUNG');
+
+  useEffect(() => {
+    font.load().then(function () {
+      console.log('BMYEONSUNG has loaded');
+      setIsFontLoaded(true);
+    });
+  }, []);
+
   return (
     <div className='BannerVideo w-full h-screen overflow-hidden relative bg-texture'>
       <div className='absolute h-screen w-full left-1/2'>
@@ -12,10 +25,16 @@ function BannerVideo() {
           muted
         >
           <source src={video_webm} type='video/webm' />
-          {/* <source src={video} type='video/mp4' /> */}
+          <source src={video} type='video/mp4' />
         </video>
       </div>
-      <div className='w-full h-full flex justify-center items-center'>
+      <div
+        className='w-full h-full flex justify-center items-center'
+        style={{
+          opacity: isFontLoaded ? 1 : 0,
+          transition: 'opacity 0.3s ease',
+        }}
+      >
         <div className='text-white text-center'>
           <div className='text-6xl leading-none font-semibold'>KEEP</div>
           <div className='text-6xl leading-none font-semibold'>CALM</div>
